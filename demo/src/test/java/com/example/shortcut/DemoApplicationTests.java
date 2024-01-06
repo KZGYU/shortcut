7ac7b022174708b04d25f82e08d878b65efb35d6
KZGYU <adventcdl777@yahoo.co.jp>
2024-01-06 21:38:17 +0900

    ショートカット一覧集

---
+++ demo/src/test/java/com/example/shortcut/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
