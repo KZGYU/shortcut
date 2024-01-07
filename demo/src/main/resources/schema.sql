f723e9d4d6fb341305e97ae4e915bbf4e1c7a8e0
KZGYU <adventcdl777@yahoo.co.jp>
2024-01-07 12:15:55 +0900

    sql修正したがエラーが発生する

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS shortcuts (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(255),
-    content VARCHAR(5000),
-    command VARCHAR(255)
+CREATE TABLE IF NOT EXISTS shortcuts
+(
+   id INT NOT NULL PRIMARY KEY,
+   name VARCHAR (255),
+   content VARCHAR (5000),
+   command VARCHAR (255)
 );
