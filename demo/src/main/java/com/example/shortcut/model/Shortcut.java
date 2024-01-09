4458e8e6801ef9dfca359b40fadb80129048df29
KZGYU <adventcdl777@yahoo.co.jp>
2024-01-09 12:20:26 +0900

    eclipseショートカット一覧の更新・削除・追加を入れた

--- demo/src/main/java/com/example/shortcut/model/Shortcut.java
+++ demo/src/main/java/com/example/shortcut/model/Shortcut.java
@@ -1,24 +1,82 @@
-//アプリケーションのデータモデルを表すJavaクラス
+
+//    @Entityアノテーションは、このクラスがエンティティであることを示します。
+//    @Table(name = "shortcuts")は、このエンティティがマッピングされるデータベースのテーブル名を指定します。
+//    @Idと@GeneratedValue(strategy = GenerationType.IDENTITY)は、主キーとその自動生成戦略を定義します。
+//    @Column(name = "...")アノテーションは、各フィールドがデータベースのどのカラムに対応するかを指定します。
+
+
 
 package com.example.shortcut.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity // このクラスがエンティティであることを示す
+@Entity
+@Table(name = "shortcuts") // データベースのテーブル名を指定
 public class Shortcut {
 
-    @Id // 主キーを示す
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDの自動生成戦略
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name; // ショートカットの名前
-    private String content; // ショートカットの内容
-    private String command; // ショートカットのコマンド
+    @Column(name = "name") // データベースのカラム名を指定
+    private String name;
 
-    // コンストラクタ、ゲッター、セッターなどは省略
+    @Column(name = "content")
+    private String content;
 
-    // 必要に応じて、コンストラクタ、ゲッター、セッターをここに追加してください。
+    @Column(name = "command")
+    private String command;
+
+    // コンストラクタ
+    public Shortcut() {
+    }
+
+    // ゲッターとセッター
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    // toStringメソッド（オプショナル）
+    @Override
+    public String toString() {
+        return "Shortcut{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                ", command='" + command + '\'' +
+                '}';
+    }
 }
