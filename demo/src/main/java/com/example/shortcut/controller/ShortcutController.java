5c3658ae7d5975f0cd43f3c8bee2032df2b9ebfa
KZGYU <adventcdl777@yahoo.co.jp>
2024-01-11 18:59:28 +0900

    テスト

--- demo/src/main/java/com/example/shortcut/controller/ShortcutController.java
+++ demo/src/main/java/com/example/shortcut/controller/ShortcutController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,6 +40,13 @@ public class ShortcutController {
 	public String updateShortcut(@ModelAttribute("shortcut") Shortcut shortcut) {
 		shortcutService.save(shortcut); // Shortcutを保存
 		return "redirect:/eclipseShortcuts"; // "/eclipseShortcuts"にリダイレクト
+	}
+	
+	
+	@GetMapping("/shortcuts/delete/{id}")
+	public String deleteShortcut(@PathVariable Long id) {
+	    shortcutService.delete(id);
+	    return "redirect:/eclipseShortcuts"; // 削除後にショートカット一覧ページにリダイレクト
 	}
 
 	@GetMapping("/shortcuts/new") // "/shortcuts/new" URLへのGETリクエストを処理
--- demo/src/main/java/com/example/shortcut/model/Shortcut.java
+++ demo/src/main/java/com/example/shortcut/model/Shortcut.java
@@ -1,3 +1,11 @@
+
+//    @Entityアノテーションは、このクラスがエンティティであることを示します。
+//    @Table(name = "shortcuts")は、このエンティティがマッピングされるデータベースのテーブル名を指定します。
+//    @Idと@GeneratedValue(strategy = GenerationType.IDENTITY)は、主キーとその自動生成戦略を定義します。
+//    @Column(name = "...")アノテーションは、各フィールドがデータベースのどのカラムに対応するかを指定します。
+
+
+
 package com.example.shortcut.model;
 
 import jakarta.persistence.Column;
@@ -9,23 +17,32 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "shortcuts")
-@Data // Lombokの@Dataアノテーションを使用
+@Table(name = "shortcuts") // データベースのテーブル名を指定
+@Data
 public class Shortcut {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id") // idフィールドに@Columnアノテーションを追加
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//新しいエンティティがデータベースに保存される際に、IDが自動的に生成
+    @Column(name = "id")
+    private Integer id;
 
-	@Column(name = "name")
-	private String name;
+    @Column(name = "name") // データベースのカラム名を指定
+    private String name;
 
-	@Column(name = "content")
-	private String content;
+    @Column(name = "content")
+    private String content;
 
-	@Column(name = "command")
-	private String command;
+    @Column(name = "command")
+    private String command;
 
-	// Lombokの@Dataにより、ゲッター、セッター、toStringメソッドは自動生成される
+    // toStringメソッド（オプショナル）
+    @Override
+    public String toString() {
+        return "Shortcut{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", content='" + content + '\'' +
+                ", command='" + command + '\'' +
+                '}';
+    }
 }
