bca196937e80ccba38d3506cd5ffd3aff1f2efd5
KZGYU <adventcdl777@yahoo.co.jp>
2024-01-12 18:06:10 +0900

    questionsテーブルデータ作成

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS shortcuts
    command VARCHAR (255)
    
 );
+
+CREATE TABLE IF NOT EXISTS questions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255),
+    answer_id INT,
+    FOREIGN KEY (answer_id) REFERENCES shortcuts(id)
+);
